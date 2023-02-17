956c279182843ff2c41cf0a1af3414cb1337dac5
ShyamP1112 <113552600+ShyamP1112@users.noreply.github.com>
2023-02-17 10:16:46 +0530

    Delete question4.py

--- question4.py
+++
@@ -1,4 +0,0 @@
-#How memory is managed in Python?
-
-#Memory management in python involves a private heap containing all python objects and data structures.
-# The management of this private heap is ensured internally by the python memory manager.
