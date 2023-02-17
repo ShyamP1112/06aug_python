910dc1bcd3c9e478872933caa4cd83a5ed1eab6a
ShyamP1112 <113552600+ShyamP1112@users.noreply.github.com>
2023-02-17 10:14:56 +0530

    Delete question14.py

--- question14.py
+++
@@ -1,4 +0,0 @@
-# What are negative indexes and why are they used?
-
-str="I am Alish"
-print(str[-1])#you can use negative indexing as your advantage when you want to pick values from the end (right side) of an iterable.
