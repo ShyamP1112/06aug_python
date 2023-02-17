7494893ea3b557bfef752138f19d190c3638d621
ShyamP1112 <113552600+ShyamP1112@users.noreply.github.com>
2023-02-17 10:17:26 +0530

    Delete question8.py

--- question8.py
+++
@@ -1,7 +0,0 @@
-#write a python program to test whether a passed latter is vowel or not?
-
-ch=input("Type a latter:")
-if ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U':
-    print("letter is vowel")
-else:
-    print("letter is not vowel")  
